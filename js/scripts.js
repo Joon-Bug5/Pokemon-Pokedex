ea2a45568f39beac42ffd18c39f40500169279f6
David <davidwonchung@gmail.com>
2021-10-14 15:48:55 -0500

    Added Pokemon List with name, height, and type

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-alert('Hello world'); // making comments for one line
-let favoriteFood = 'Pizza'; /* making comments for multiple lines */
-// Any highlighted lines you can make into comments using control /
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'] },
+  { name: 'Charmander', height: 0.6, type: 'Fire' },
+  { name: 'Squirtle', height: 0.5, type: 'Water' },
+  { name: 'Pikachu', height: 0.4, type: 'Electric' },
+  { name: 'Articuno', height: 1.7, type: ['Ice', 'Flying'] },
+  { name: 'Zapdos', height: 1.6, type: ['Electric', 'Flying'] },
+  { name: 'Moltres', height: 2, type: ['Fire', 'Flying'] },
+  { name: 'Mew', height: 0.4, type: 'Psychic' },
+  { name: 'Mewtwo', height: 2, type: 'Psychic' },
+  { name: 'Eevee', height: 0.3, type: 'Normal' }
+];
