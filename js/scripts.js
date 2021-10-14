53ceee5e0f5a88953a5855489542cbe269012489
David <davidwonchung@gmail.com>
2021-10-14 12:25:15 -0500

    added alert, favorite food and document.write

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,4 @@
-alert('Hello world');
+alert('Hello world'); // making comments for one line
+let favoriteFood = 'Pizza'; /* making comments for multiple lines */
+// Any highlighted lines you can make into comments using control /
+document.write(favoriteFood);
