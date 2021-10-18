e2bf60a04a9ee3c50146d25886cdca3e8c103fdc
David <davidwonchung@gmail.com>
2021-10-18 14:21:04 -0500

    Updated CSS, HTML, JS
    CSS - Added more style or organized the list of pokemons vertically and centered in page
    
    HTML - Changed title name
    
    JS - Listed pokemons with loop and one conditional with largest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,16 @@ let pokemonList = [ // Use solid braces for arrays. !!!! Use LET if variable wil
   { name: 'Pikachu', height: 0.4, type: 'Electric' },
   { name: 'Articuno', height: 1.7, type: ['Ice', 'Flying'] }, // Using solid braces within the curly braces due to separate arrays.
   { name: 'Zapdos', height: 1.6, type: ['Electric', 'Flying'] },
-  { name: 'Moltres', height: 2, type: ['Fire', 'Flying'] },
+  { name: 'Moltres', height: 1.9, type: ['Fire', 'Flying'] },
   { name: 'Mew', height: 0.4, type: 'Psychic' },
   { name: 'Mewtwo', height: 2, type: 'Psychic' },
   { name: 'Eevee', height: 0.3, type: 'Normal' }
 ];
 // You can choose which line you are going to comment with CTRL /
 // && (First False, Last Truth), || (First Truth, First False)
-for (let list = 0; list < pokemonList.length; list++){
-  document.write(pokemonList[list].name + '- (Height: ' +pokemonList[list].height + ' m),' + ' ');
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write('<br>' + '<p>' + ('<span>' + pokemonList[i].name + '</span>') + ' - (Height: ' + pokemonList[i].height + ' m)' + ' ' + '</p>'); // Can add HTML elements within JS.
+  if (pokemonList[i].height >= 2) {
+    document.write(' - "Wow, that\'s big!" ');
+  }
 }
