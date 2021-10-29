8d3114691f4dcb220950b6f7844f97123ba3be33
David <davidwonchung@gmail.com>
2021-10-28 21:15:24 -0500

    Using Ajax to send and fetch data from external API
    Changed pokemonList to external page, created a function to fetch from external page to return a JSON response to show name of 150pokemon and updated to have console.log show details of imageurl, height, types.
    Also figured out how to capitalize first letter of each name, since from external source, name is all lowercased

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,6 @@
 let pokemonRepository = (function () {
-  let pokemonList = [ // Use solid braces for arrays. !!!! Use LET if variable will change, or CONST if variable will not change
-    { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'] }, // Curly braces are for objects (unordered list of data).
-    { name: 'Charmander', height: 0.6, type: 'Fire' }, // Use single quotes for string and number has no quotes.
-    { name: 'Squirtle', height: 0.5, type: 'Water' },
-    { name: 'Pikachu', height: 0.4, type: 'Electric' },
-    { name: 'Articuno', height: 1.7, type: ['Ice', 'Flying'] }, // Using solid braces within the curly braces due to separate arrays.
-    { name: 'Zapdos', height: 1.6, type: ['Electric', 'Flying'] },
-    { name: 'Moltres', height: 2, type: ['Fire', 'Flying'] },
-    { name: 'Mew', height: 0.4, type: 'Psychic' },
-    { name: 'Mewtwo', height: 2, type: 'Psychic' },
-    { name: 'Eevee', height: 0.3, type: 'Normal' }];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add (pokemon) { // adding to the last list of the array
     pokemonList.push(pokemon);
@@ -23,7 +14,7 @@ let pokemonRepository = (function () {
     let mainPokemon = document.querySelector('.pokemon-list');// selecting the class in the HTML, need '' quotes and . to choose the class same as CSS
     let listPokemon = document.createElement('li'); // creates the list element
     let button = document.createElement('button'); // creates the button element
-    button.innerText = pokemon.name; // name of the button in the inner text of the button
+    button.innerText = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1); // name of the button in the inner text of the button
     button.classList.add('button-class'); // adding the class, which then you can create the style of the button on class
     listPokemon.appendChild(button);// showing the list as a button that was just created
     mainPokemon.appendChild(listPokemon);// showing the list in the class of the selector
@@ -37,23 +28,51 @@ let pokemonRepository = (function () {
   }
 
   function showDetails (pokemon) {
-    console.log(pokemon.name);
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
+
+  function loadList () {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails (item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
   }
 
   return { // returns the information of once added.
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })(); // IIFE is within local only ().
 
-const Psyduck = { name: 'Psyduck', height: 0.8, type: 'Water' };
-const Vulpix = { name: 'Vulpix', height: 0.6, type: 'Fire' };
-const Ninetales = { name: 'Ninetales', height: 1.1, type: 'Fire' };
-pokemonRepository.add(Psyduck);
-pokemonRepository.add(Vulpix);
-pokemonRepository.add(Ninetales);
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
